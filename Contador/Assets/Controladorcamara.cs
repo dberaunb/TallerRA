89caf43e8bf4b72a44d8d7c972f5aee9ea8a206e
Carlos <carlos_Rodriguez15@usmp.pe>
2018-08-20 11:58:17 -0500

    Avances anteriores

---
+++ Contador/Assets/Controladorcamara.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controladorcamara : MonoBehaviour {
+
+    public GameObject jugador;
+    private Vector3 positionrelative;
+
+	// Use this for initialization
+	void Start () {
+        positionrelative =  transform.position - jugador.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        transform.position = jugador.transform.position + positionrelative;
+
+	}
+}
---
+++ Lunes 13-08-18/Assets/scripts/Correr.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Correr : MonoBehaviour {
+    Animator animator;
+    public float speed;
+    Vector3 MoveVector;
+    float directionx;
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator> ();
+        rb = GetComponent<Rigidbody>();
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        transform.Translate(0, 0, 0.1f);
+
+        if (Input.GetKey(KeyCode.W)) { 
+ 
+            animator.SetBool("isWalking", true);
+            animator.SetBool("isIdle", false);
+
+        }
+        else if (Input.GetKey(KeyCode.S)) {
+
+            animator.SetBool("isWalking", true);
+            animator.SetBool("isIdle", false);
+
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isIdle", true);
+        }
+
+
+       
+
+    }
+    
+    
+    
+
+
+
+}
