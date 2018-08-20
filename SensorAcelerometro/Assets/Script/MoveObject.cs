08dc9daf44bc753e42fdbecf16fdcda9256f07cf
Marilyn1206 <marilyn_valverde@usmp.pe>
2018-08-20 11:29:04 -0500

    Movimiento de Objeto

---
+++ SensorAcelerometro/Assets/Script/MoveObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        GetComponent<Rigidbody>().MovePosition(transform.position + transform.forward * Time.deltaTime * Input.GetAxis("Vertical"));
+
+        Quaternion quaternion = Quaternion.Euler(0, 90 * Input.GetAxis("Horizontal") * Time.deltaTime, 0);
+
+        GetComponent<Rigidbody>().MoveRotation(transform.rotation * quaternion);
+
+        
+    }
+}
