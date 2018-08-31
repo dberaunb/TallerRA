9caf36f6db6e9ae0340de96263c9bc10cd26cea6
Carlos <carlos_Rodriguez15@usmp.pe>
2018-08-31 13:59:42 -0500

    Avance

---
+++ NuevoYeah v1.0/Assets/Scripts/Movimiento.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour {
+    public Animator an;
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb =  GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+        //an.transform.position = an.transform.position + an.transform.forward;
+        
+        
+	}
+}
---
+++ NuevoYeah v1.0/Assets/Scripts/WP_Actor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WP_Actor : MonoBehaviour {
+    public AudioSource AS;
+    Animator an;
+    float speed = 0.5f;
+    public Transform target;
+    
+
+	// Use this for initialization
+	void Start () {
+        an = GetComponent<Animator>();
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Waypoint")
+        {
+            speed = 0;
+            an.SetBool("isIdle", true);
+            AS.Play();
+            
+        }
+    }
+
+     
+}
