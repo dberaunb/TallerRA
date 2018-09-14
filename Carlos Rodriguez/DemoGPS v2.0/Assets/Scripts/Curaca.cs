b2e629dcebe495a6adf968d705b90865671e21eb
Carlos <carlos_Rodriguez15@usmp.pe>
2018-09-14 12:41:51 -0500

    Jueves 13-09

---
+++ Carlos Rodriguez/DemoGPS v2.0/Assets/Scripts/Curaca.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Curaca : MonoBehaviour {
+    public GameObject curaca;
+    public AudioSource As;
+    Animator an;
+	// Use this for initialization
+	void Start () {
+        an = GetComponent<Animator>();
+        As.Play();
+        
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (As.isPlaying)
+        {
+            an.SetBool("isTalking",true);
+        }
+        else
+        {
+            an.SetBool("isTalking", false);
+        }
+	}
+}
